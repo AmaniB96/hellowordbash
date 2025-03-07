f8d814efa9da1ef1d23b5714cfa3dff7e2e6ff4b
amani <bataganiramani@gmail.com>
2025-03-07 11:06:53 +0100

    First commit

---
+++ arbo.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+mkdir -p Public/{css/{footer,header,main},img,js}
+touch Public/css/footer/style.css
+touch Public/css/header/style.css
+touch Public/css/main/style.css
+touch Public/img/img1.png
+touch Public/img/img2.png
+touch Public/js/main.js
+touch Public/index.html
+
+tree.com //f
---
+++ creation.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+echo "Nom du fichier à créer :"
+read nom
+
+touch $nom
+
+echo "Fichier $nom créé avec succès"
+
+
+
+
